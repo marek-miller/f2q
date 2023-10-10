93f2b6471efb8d036ba5370d3413c695a9150a08
Marek Miller <mlm@math.ku.dk>
2023-10-10 10:15:57 +0200

    Update example, use rand

--- examples/jordan_wigner_64qb.rs
+++ examples/jordan_wigner_64qb.rs
@@ -17,36 +17,32 @@ use f2q::{
     Pairs,
     Terms,
 };
+use rand::Rng;
 
 const ORBITAL_MAX_IDX: usize = 64;
-const DELTA: f64 = 0.012_345;
 
 fn main() {
-    let now = Instant::now();
-
-    let mut coeff = DELTA;
+    let mut rng = rand::thread_rng();
 
     let orbitals = Orbital::gen_range(0..ORBITAL_MAX_IDX).collect::<Vec<_>>();
     let orbital_pairs = Pairs::new(&orbitals).collect::<Vec<_>>();
 
+    let now = Instant::now();
     let mut fermi_sum = SumRepr::new();
-    fermi_sum.add(Integral::Constant, 1.0);
 
+    fermi_sum.add(Integral::Constant, 1.0);
     for code in orbital_pairs
         .iter()
         .flat_map(|(&p, &q)| Integral::one_electron(p, q))
     {
-        fermi_sum.add(code, coeff);
-        // this is completely arbitrary
-        coeff += DELTA;
+        // the coefficient is completely arbitrary
+        fermi_sum.add(code, rng.gen_range(-1.0..1.0));
     }
-
     for code in Pairs::new(&orbital_pairs)
         .flat_map(|((&p, &q), (&r, &s))| Integral::two_electron((p, q), (r, s)))
     {
-        fermi_sum.add(code, coeff);
-        // this is completely arbitrary
-        coeff += DELTA;
+        // the coefficient is completely arbitrary
+        fermi_sum.add(code, rng.gen_range(-1.0..1.0));
     }
 
     println!(
