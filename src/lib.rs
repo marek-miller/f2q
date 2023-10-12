83a883126cd1ef1a1922fdad12301b0b934926d4
Marek Miller <mlm@math.ku.dk>
2023-10-12 21:43:37 +0200

    Add module math

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::{
 
 use terms::SumRepr;
 
+pub mod math;
 pub mod maps;
 pub mod qubit;
 pub mod secnd;
@@ -30,7 +31,7 @@ pub mod prelude {
             SumRepr,
         },
         Code,
-        Pairs,
+        math::Pairs,
         Terms,
     };
 }
@@ -78,42 +79,3 @@ impl Display for Error {
 }
 
 impl std::error::Error for Error {}
-
-/// Iterate over all pairs in a slice.
-#[derive(Debug)]
-pub struct Pairs<'a, T> {
-    data: &'a [T],
-    i:    usize,
-    j:    usize,
-}
-
-impl<'a, T> Pairs<'a, T> {
-    pub fn new(data: &'a [T]) -> Self {
-        Self {
-            data,
-            i: 0,
-            j: 0,
-        }
-    }
-}
-
-impl<'a, T> Iterator for Pairs<'a, T> {
-    type Item = (&'a T, &'a T);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i >= self.data.len() {
-            return None;
-        }
-
-        let out = (&self.data[self.i], &self.data[self.j]);
-
-        self.j += 1;
-
-        if self.j >= self.data.len() {
-            self.j = 0;
-            self.i += 1;
-        }
-
-        Some(out)
-    }
-}
