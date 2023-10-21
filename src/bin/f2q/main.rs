433365e53c3c70e69cee0b95a9eded8b8fcebd8e
Marek Miller <mlm@math.ku.dk>
2023-10-21 22:21:03 +0200

    Reformat

--- src/bin/f2q/main.rs
+++ src/bin/f2q/main.rs
@@ -7,35 +7,13 @@ use clap::Parser;
 
 static CLI: OnceLock<Cli> = OnceLock::new();
 
-fn main() -> ExitCode {
-    env_logger::init();
-
-    log::debug!("Parsing command line arguments");
-    let cli = CLI.get_or_init(Cli::parse);
-
-    if cli.verbose {
-        log::debug!("--verbose flag set");
-    }
-
-    match main_exec(cli) {
-        Ok(()) => {
-            log_or_eprintln_if_verbose("Done.");
-            log::info!("Exit (0)");
-            ExitCode::from(0)
+fn log_or_eprintln_if_verbose(msg: &str) {
+    if let Some(cli) = CLI.get() {
+        if cli.verbose {
+            eprintln!("{msg}");
+        } else {
+            log::info!("{msg}");
         }
-        Err(err) => {
-            log::error!("{err}");
-            let exit_code = u8::from(&err);
-            log::error!("Exit ({})", exit_code);
-            ExitCode::from(exit_code)
-        }
-    }
-}
-
-fn main_exec(cli: &Cli) -> Result<(), Error> {
-    match &cli.command {
-        Commands::Generate(args) => command::generate(args),
-        Commands::Convert(args) => command::convert(args),
     }
 }
 
@@ -50,12 +28,31 @@ use args::{
 
 mod command;
 
-fn log_or_eprintln_if_verbose(msg: &str) {
-    if let Some(cli) = CLI.get() {
-        if cli.verbose {
-            eprintln!("{msg}");
-        } else {
-            log::info!("{msg}");
-        }
+fn main() -> ExitCode {
+    env_logger::init();
+
+    log::debug!("Parsing command line arguments");
+    let cli = CLI.get_or_init(Cli::parse);
+
+    if cli.verbose {
+        log::debug!("--verbose flag set");
+    }
+
+    if let Err(e) = main_exec(cli) {
+        log::error!("{e}");
+        let exit_code = u8::from(&e);
+        log::error!("Exit ({})", exit_code);
+        ExitCode::from(exit_code)
+    } else {
+        log_or_eprintln_if_verbose("Done.");
+        log::info!("Exit (0)");
+        ExitCode::from(0)
+    }
+}
+
+fn main_exec(cli: &Cli) -> Result<(), Error> {
+    match &cli.command {
+        Commands::Generate(args) => command::generate(args),
+        Commands::Convert(args) => command::convert(args),
     }
 }
